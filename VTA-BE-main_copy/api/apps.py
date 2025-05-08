852dcdb7918f9ad2e887066d7c5e91500bb3e75d
Julia Kotovich <37921248+Julia-Kotovich@users.noreply.github.com>
2025-05-08 22:22:09 +0200

    fix model path

--- VTA-BE-main_copy/api/apps.py
+++ VTA-BE-main_copy/api/apps.py
@@ -15,7 +15,7 @@ class ApiConfig(AppConfig):
     capstone_qa_vta = None
 
     if os.path.exists(model_root_dir):
-        model_path = os.path.abspath('./vta_qa_model/model/vta_model_bert_7alpha')
+        model_path = os.path.abspath('./VTA-tools/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf')
         tokenizer = BertTokenizer.from_pretrained(model_path, local_files_only=True)
         context_path = os.path.abspath('./vta_qa_model/training/newdataset/vista.json')
         model = BertForQuestionAnswering.from_pretrained(model_path, local_files_only=True)
